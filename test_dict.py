162e0e3354aca9c1c7d08cc2fb5e254043a00ef9
jwg4 <jwg4@users.noreply.github.com>
2017-06-02 15:28:12 +0000

    Create test file.

---
+++ test_dict.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestDictEquality(unittest.TestCase):
+    a = {'foo': 'bar'}
+    b = {u'foo': 'bar'}
+    
+    def test_using_equals(self):
+        self.assertTrue(self.a == self.b)
+
+    def test_using_assertEqual(self):
+        self.assertEqual(self.a, self.b)
